a2f9aabef7dba2f8270344f2688b3ab6d75d987e
VinhPV <vinhpvse172511@fpt.edu.vn>
2025-02-23 23:24:35 +0700

    add completed

--- vaccine_care/src/pages/staff/completed/Completed.jsx
+++ vaccine_care/src/pages/staff/completed/Completed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Card } from "antd";
+import "./Completed.css";
+import { useState, useEffect } from "react";
+import { FaRegCircleCheck } from "react-icons/fa6";
 
 const Completed = () => {
-  return (
-    <div>Completed</div>
-  )
-}
+  const [showTick, setShowTick] = useState(false);
 
-export default Completed
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowTick(true);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="completed">
+      <Card className="completed_card">
+        <div className={`completed_icon_container ${showTick ? "show" : ""}`}>
+          <FaRegCircleCheck className="completed_icon" />
+        </div>
+        <h2>Hoàn thành!</h2>
+        <p>Quy trình đã được xử lý thành công.</p>
+      </Card>
+    </div>
+  );
+};
+
+export default Completed;
