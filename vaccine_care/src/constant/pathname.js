14e99c35ec868fc5af719798bb206c9a66fe8698
hoanglhse172418 <hoanglhse172418@fpt.edu.vn>
2025-02-25 21:17:36 +0700

    update

--- vaccine_care/src/constant/pathname.js
+++ vaccine_care/src/constant/pathname.js
@@ -29,15 +29,13 @@ export const PATH_NAME = {
   IN4: "/in4",
   NEWLIST: "/newlist",
   NEWLDETAIL: "/news/:id",
-};
-    HOME: "/",
-    ADMIN: {
-        DASHBOARD: "/admin/dashboard",
+
+  DASHBOARD: "/admin/dashboard",
         STAFF: "/admin/staff",
         VACCINE: "/admin/vaccine",
         CHILD: "/admin/child",
         DISEASE: "/admin/disease",
         PAYMENT_HISTORY: "/admin/payment-history",
         PROFILE: "/admin/profile"
-    }
-}
+};
+    
