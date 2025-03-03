92de4c9408a9a69f4ad3526def8638d81b0efccf
khangper <89836419+khangper@users.noreply.github.com>
2025-03-03 07:27:31 +0700

    fix succesbookingsuccesbooking

--- vaccine_care/src/pages/cutomer/Success/SuccesBooking/SuccesBooking.jsx
+++ vaccine_care/src/pages/cutomer/Success/SuccesBooking/SuccesBooking.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function SuccesBooking() {
   return (
     <div className='SuccessRegis-container'>
-      <div className='SuccessRegis-title'>Chúc mừng bạn đã tạo tài khoản cho bé thành công</div>
+      <div className='SuccessRegis-title'>Chúc mừng bạn đã đặt lịch cho bé thành công</div>
       <Link to='/'><div className='SuccessRegis-button'>
         Trang chủ
       </div></Link>
